487ab93837ce25e2a92179ae50dafd48e92af592
Dragos Popescu <dragos.andreip@yahoo.com>
2018-10-06 02:26:44 +0300

    Fixed catastrophic typo in setup.sh. Oops.

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ touch local_credentials.py
 
 printf "BOT_TOKEN = 'Enter Token Here'\n\n" > local_credentials.py
 
-printf "CHANNEL_ID = 'Enter Channel ID Here\n\n" >> local_credentials.py
+printf "CHANNEL_ID = 'Enter Channel ID Here'\n\n" >> local_credentials.py
 
 printf "PYTHON_ALIAS = 'python' #only change if you have multiple python installations" >> local_credentials.py
 
