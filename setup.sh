78a34134a0eea92a17147a0bb22a19b7130e2ace
Zach <z61@zeesixtyone.com>
2018-10-16 01:31:13 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,25 +2,9 @@ echo "Installing Required Packages..."
 
 pip3 install -U -r ./requirements.txt
 
-configfile="configs.py"
+echo "Creating environment configuration file"
 
-echo "Creating $configfile ..."
+cp .env.example .env
 
-touch $configfile
-
-printf "BOT_TOKEN = 'Enter Token Here'\n\n" > $configfile
-
-printf "CHANNEL_ID = 'Enter Channel ID Here'\n\n" >> $configfile
-
-printf "PYTHON_ALIAS = 'python' #only change if you have multiple python installations\n\n" >> $configfile
-
-printf "DISK_LOGS_ENABLED = True\n\n" >> $configfile
-
-printf "initial_display_output = True\n\n" >> $configfile
-
-printf "initial_path = ''\n\n" >> $configfile
-
-printf "discord_logs_enabled = False\n\n" >> $configfile
-
-echo "Done. Please fill the required fields in local_credentials.py"
+echo "Done. Please fill the required fields in .env"
 
