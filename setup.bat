13017b650d1dcd5dcb6368274fa87fbbfd3623b0
Zach <z61@zeesixtyone.com>
2018-10-16 01:31:23 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,36 +4,16 @@ echo Installing required packages.
 
 pip install -U -r .\requirements.txt
 
-SET configfile="configs.py"
+SET exampleFile=".env.example"
+SET newFile=".env."
 
-echo Creating %configfile%
+echo Creating environment configuration file
 
-echo BOT_TOKEN = 'Enter Token Here' > %configfile%
-
-echo. >> %configfile%
-
-echo CHANNEL_ID = 'Enter Channel ID here' >> %configfile%
-
-echo. >> %configfile%
-
-echo PYTHON_ALIAS = 'python' #only change if you have multiple python installations >> %configfile%
-
-echo. >> %configfile%
-
-echo DISK_LOGS_ENABLED = True >> %configfile%
-
-echo. >> %configfile%
-
-echo initial_display_output = True >> %configfile%
-
-echo. >> %configfile%
-
-echo initial_path = "" >> %configfile%
-
-echo. >> %configfile%
-
-echo discord_logs_enabled = False >> %configfile%
-
-echo Done. Please fill the required field in %configfile%
+IF EXIST %newFile% (
+  echo Configuration file already exists
+) ELSE (
+  copy %exampleFile% %newFile%
+)
+echo Done. Please fill the required field in %newFile%
 
 pause
