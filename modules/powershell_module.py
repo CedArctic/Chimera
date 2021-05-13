4e168954b1762d85c741d5538751a969d3ff6931
Aethese <59108437+Aethese@users.noreply.github.com>
2021-05-13 12:42:53 -0500

    Update powershell_module.py

--- modules/powershell_module.py
+++ modules/powershell_module.py
@@ -9,7 +9,7 @@ import os, asyncio, configs
 async def powershell(ctx, cmnd):
     if configs.operating_sys == "Windows":
         await ctx.send("Executing in powershell: " + cmnd)
-        cmnd_result = os.popen("powershell {}".format(cmnd)).read()
+        cmnd_result = os.popen(f"powershell {cmnd}").read()
         if configs.initial_display_output:
             await ctx.say(cmnd_result)
     else:
